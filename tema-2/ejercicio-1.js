e4ce4d8ab19e797b6b1c62a7fe074c8b4da63d0d
Yanmarex <jhonnyjoestarparte7@gmail.com>
2022-07-15 18:36:17 -0400

    add date

--- tema-2/ejercicio-1.js
+++ tema-2/ejercicio-1.js
@@ -1,7 +1,7 @@
-const lista = ["Luis", 27, false, 
+const lista = ["Luis", 27, false,new Date(1995, 2, 5), 
 {
     titulo: "Bleach",
     autor: "Tite Kubo",
-    fecha: 2001,
+    fecha: new Date(2001, 7, 15),
     url: "https://www.amazon.com/Bleach-Box-Set-Vol-1-21/dp/1421526107/ref=asc_df_1421526107/?tag=hyprod-20&linkCode=df0&hvadid=385184279251&hvpos=&hvnetw=g&hvrand=17682743335757279442&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9067609&hvtargid=pla-404766145399&psc=1&tag=&ref=&adgrpid=78921990659&hvpone=&hvptwo=&hvadid=385184279251&hvpos=&hvnetw=g&hvrand=17682743335757279442&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9067609&hvtargid=pla-404766145399"
 }];
